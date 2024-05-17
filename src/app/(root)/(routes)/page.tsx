03d9683aa43ebe0e8b95a0f867623c97419d0918
Daniel <dmunier27@gmail.com>
2024-05-17 00:24:02 -0300

    credentialsForm in /

---
+++ src/app/(root)/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { CredentialsForm } from "@/components/credentialsForm";
+
+export default function Home() {
+
+  
+
+
+  // Formulario para testar back-end
+  return (
+    <CredentialsForm/>  
+    
+  )
+}
