39ad79d06adce08dccc012a0e611b3df1b8170d3
Daniel <dmunier27@gmail.com>
2024-05-18 03:37:10 -0300

    credentialsForm setup

--- src/components/credentialsForm.tsx
+++ src/components/credentialsForm.tsx
@@ -1,46 +1,127 @@
-"use client"
+"use client";
 
-import { signIn } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
-import React, { Component, useState } from 'react'
+import axios from "axios";
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { Component, useCallback, useState } from "react";
+import Input from "./Input";
+import { getServerSession } from "next-auth";
+import authConfig from "@/lib/auth";
 
 interface CredentialsFormProps {
-    csrfToken?: string
+  csrfToken?: string;
 }
 
-export async function CredentialsForm(props: CredentialsFormProps) {
-    const router = useRouter()
-    const [error, setError] = useState<string | null>(null)
 
-    const handleSubmit = async (e: any) => {
-        e.preventDefault()
 
-        const data = new FormData(e.currentTarget)
+export function CredentialsForm(props: CredentialsFormProps) {
+  
 
-        const signInResponse = await signIn("credentials", {
-            email: data.get("email"),
-            password: data.get("password"),
-            redirect: false
-        }
-        )
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const [variant, setVariant] = useState("login");
+  const [nickname, setNickname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-        
-        if(signInResponse && !signInResponse.error) {
-            router.push(`/${data.get("email")}`)
-        } else {
-            console.log("Something went wrong")
-            setError("Something went wrong " + signInResponse?.error )
-        }
-        }
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "signup" : "login"
+    );
+  }, []);
 
-        return (
-            <form onSubmit={handleSubmit}>
-                <input type="email" name="email" placeholder="Email" />
-                <input type="password" name="password" placeholder="Password" />
-                <button type="submit">Login</button>
-                {error && <p>{error}</p>}
-            </form>
-        )
+  const login = useCallback(async () => {
 
+    try {
+      const signInData = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      })
+
+      console.log(signInData)
+    } catch (e) {
+      console.log("Error trying to login");
+      console.log(e);
     }
+  }, [email, password]);
 
+  const register = useCallback(async () => {
+    try {
+      const registerData = await axios.post("/api/register", {
+        email,
+        nickname,
+        password,
+      });
+      console.log("Successfull register");
+      console.log(registerData)
+      router.push("/");
+    } catch (e) {
+      console.log("Error trying to register");
+      console.log(e);
+    }
+  }, [email, nickname, password]);
+
+  return (
+    <div className="relative h-full w-full">
+      <div className="bg-slate-400 w-full h-full lg:bg-opacity-90">
+        <div className="flex justify-center">
+          <div className="px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h1 className="text-black text-5lx mb-5 font-semibold">
+              {variant === "login" ? "Sign in" : "Create an account"}
+            </h1>
+
+            <div className="flex flex-col gap-4">
+              {variant === "signup" && (
+                <Input
+                  label="Name"
+                  onChange={(ev: any) => setNickname(ev.target.value)}
+                  id="name"
+                  type="name"
+                  value={nickname}
+                />
+              )}
+
+              <Input
+                label="Email"
+                onChange={(ev: any) => setEmail(ev.target.value)}
+                id="email"
+                type="email"
+                value={email}
+              />
+
+              <Input
+                label="Password"
+                onChange={(ev: any) => setPassword(ev.target.value)}
+                id="password"
+                type="password"
+                value={password}
+              />
+            </div>
+            <button
+              onClick={variant === "login" ? login : register}
+              className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover: bg-red-700 transition"
+            >
+              {variant === "login" ? "Login" : "Register"}
+            </button>
+
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center"></div>
+
+            <p className="text-neutral-500 mt-5">
+              {variant === "login"
+                ? "Registre-se agora "
+                : "Já possui uma conta? "}
+              <span
+                onClick={toggleVariant}
+                className="text-white cursor-pointer"
+              >
+                {variant === "login" ? "Sign up" : "Login"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
