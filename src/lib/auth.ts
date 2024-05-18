d4d23da8089524d272d060a84848eab5be85b0bc
Daniel <dmunier27@gmail.com>
2024-05-18 03:37:57 -0300

    credentials auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,12 @@
 import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import prismadb from "@/lib/prismadb"
+import GithubProvider from "next-auth/providers/github"
+
 
 
 export const authConfig: NextAuthOptions = {
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [
         CredentialsProvider({
             name: "Sign in",
@@ -19,16 +23,36 @@ export const authConfig: NextAuthOptions = {
             },
             async authorize(credentials) {
                 console.log(credentials)
-                
-                if(credentials) {
+                if(!credentials?.email || !credentials?.password) {
+                    throw new Error("Email e senha são obrigatórios")
+                }
+                const user = await prismadb.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
 
+                if(!user || !user.hashedPassword) {
+                    throw new Error("Email não registrado")
+                }
+                
+                const isCorrectPassword = user.hashedPassword === credentials.password ? user : false
+
+                if(!isCorrectPassword) {
+                    throw new Error("Senha incorreta")
                 }
 
                 return null
 
             }
+        }),
+        GithubProvider({
+            clientId: process.env.GITHUB_ID || "",
+            clientSecret: process.env.GITHUB_SECRET || ""
         })
 
-    ]}
+    ]
+    
+}
 
     export default authConfig
