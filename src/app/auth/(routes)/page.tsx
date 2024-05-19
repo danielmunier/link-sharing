c7689f213f2193a1d63058c00c04218736c97c10
Daniel <dmunier27@gmail.com>
2024-05-19 01:55:42 -0300

    signin route

--- src/app/auth/(routes)/page.tsx
+++ src/app/auth/(routes)/page.tsx
@@ -5,6 +5,9 @@ export default async function Home() {
   return (
     <div className="bg-gray-900 text-white min-h-screen p-5">
       <Navbar/>
+      <div className="flex items-center justify-center h-min-screen">
+      <CredentialsForm/>
+      </div>
     </div>
   );
 }
