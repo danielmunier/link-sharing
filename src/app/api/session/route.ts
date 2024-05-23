d04ae2120a8daae421662edcb0779d8f5ac8687c
Daniel <dmunier27@gmail.com>
2024-05-23 00:24:34 -0300

    socialmedia create

--- src/app/api/session/route.ts
+++ src/app/api/session/route.ts
@@ -1,17 +1,82 @@
-import authConfig from "@/lib/auth";
-import serverAuth from "@/lib/serverAuth";
-import { NextApiRequest, NextApiResponse } from "next";
-import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+// import bcrypt from "bcrypt"
+import { NextApiRequest, NextApiResponse } from 'next'
+import prismadb from "@/lib/prismadb"
+import * as z from "zod"
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const { currentUser } = await serverAuth(req, res);
-    const session = await getServerSession(req,res, authConfig)
-    return NextResponse.json({ currentUser: session });
-  } catch (e) {
-    console.log(e);
 
-    return NextResponse.json({ message: "Error trying to get session" }, {status: 400});
-  }
+const userSchema = z.object({
+    username: z
+    .string()
+    .min(1, "Username is required")
+    .max(100)
+    .refine((username) => /^[\w-]+$/.test(username), {
+      message: "Username must only contain letters, numbers, underscores, and hyphens",
+      path: ["username"],
+    }),
+    email: z.string().min(1, "Email is required").email("Invalid email"),
+    password: z
+      .string()
+      .min(1, "Password is required")
+      .min(8, "Password must have than 8 characters"),
+  });
+
+
+export async function POST(req: Request) {
+    const body = await req.json()
+    
+
+    try {
+        const {email, username, password} = userSchema.parse(body)
+        console.log(username)
+     
+
+
+        const existingUser = await prismadb.user.findUnique({
+            where: {
+                email: email
+            }
+        })
+
+        const existingUsername = await prismadb.user.findFirst({
+            where: {
+                username: username
+            }
+        })
+
+
+        if(existingUser) {
+            return NextResponse.json({error: "Email already in use"}, {status: 409})
+        }
+
+        if(existingUsername) {
+            return NextResponse.json({error: "Nickname already in use"}, {status: 409})
+        }
+
+       // const hashedPassword = await bcrypt.hash(password, 12)
+       const hashedPassword = password
+
+
+        const newUser = await prismadb.user.create({
+            data: {
+               email,
+               hashedPassword,
+               username,
+            }
+        })
+
+        console.log(newUser)
+        await prismadb.socialMedia.create({
+            data: {
+                
+                userId: newUser.id
+            }
+        })
+   
+        return NextResponse.json(newUser, {status: 200})
+
+    }catch(e) {
+        console.log(e)
+        return NextResponse.json({error: "Something went wrong"}, {status: 500})
+    }
 }
