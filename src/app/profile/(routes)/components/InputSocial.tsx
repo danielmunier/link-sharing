62e33c5a8ebfaab1b485cab689322c7fa14c6a6f
Daniel <dmunier27@gmail.com>
2024-05-23 03:01:47 -0300

    style: profile page

--- src/app/profile/(routes)/components/InputSocial.tsx
+++ src/app/profile/(routes)/components/InputSocial.tsx
@@ -1,34 +1,31 @@
-import { FaInstagram, FaDiscord, FaGithub, FaTiktok } from "react-icons/fa";
+import { FaInstagram, FaDiscord, FaGithub, FaTiktok, FaTwitter, FaFacebook, FaYoutube } from "react-icons/fa";
 
 export const InputSocial = ({ socialMediaName, onChange }: {socialMediaName: string, onChange: (e: any) => void}) => {
-  let socialIcons = [
-    { key: "Instagram", icon: <FaInstagram size="24" /> },
-    { key: "Discord", icon: <FaDiscord size="24" /> },
-    { key: "Github", icon: <FaGithub size="24" /> },
-    { key: "Tiktok", icon: <FaTiktok size="24" /> },
+  const socialIcons = [
+    { key: "Instagram", icon: <FaInstagram size="24" />, style: "text-red-500" },
+    { key: "Discord", icon: <FaDiscord size="24" />, style: "text-blue-500" },
+    { key: "Github", icon: <FaGithub size="24" />, style: "text-black" },
+    { key: "Tiktok", icon: <FaTiktok size="24" />, style: "text-purple-500" },
+    { key: "Twitter", icon: <FaTwitter size="24" />, style: "text-blue-500" },
+    { key: "Facebook", icon: <FaFacebook size="24" />, style: "text-blue-500" },
+    { key: "YouTube", icon: <FaYoutube size="24" />, style: "text-red-500" },
   ];
 
+  const socialIcon = socialIcons.find(icon => icon.key === socialMediaName);
+
   return (
-    <>
-      <div className="flex flex-row items-center border-solid border-black border-2">
-        {socialIcons.map((icon) => {
-          if (icon.key === socialMediaName) {
-            return (
-              <>
-                <span className="py-2 border-s ">{icon.icon}</span>
-                <input
-                  type="text"
-                  onChange={onChange}
-                  placeholder={icon.key}
-                  className="h-full w-full"
-                />
-              </>
-            );
-          } else {
-            return null
-          }
-        })}
-      </div>
-    </>
+    <div className="flex items-center space-x-2">
+      {socialIcon && (
+        <>
+          <span className={socialIcon.style}>{socialIcon.icon}</span>
+          <input
+            type="text"
+            onChange={onChange}
+            placeholder={socialIcon.key}
+            className="pl-10 pr-4 py-2 bg-gray-700 w-full rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-white placeholder-gray-400"
+          />
+        </>
+      )}
+    </div>
   );
 };
--- src/app/profile/(routes)/components/ProfileForm.tsx
+++ src/app/profile/(routes)/components/ProfileForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { InputSocial } from './InputSocial';
 
 type ProfileFormData = {
@@ -8,76 +8,70 @@ type ProfileFormData = {
   description: string;
 };
 
-
-function ProfileForm({sessionUserData}: {sessionUserData: any}) {
+function ProfileForm({ sessionUserData }: { sessionUserData: any }) {
   const [status, setStatus] = useState<string | null>(null);
-  const user = sessionUserData.user
+  const user = sessionUserData.user;
 
-    const [formData, setFormData] = useState<ProfileFormData>({
-      username: user?.name,
-      description: user?.description,
-    });
+  const [formData, setFormData] = useState<ProfileFormData>({
+    username: user?.name,
+    description: user?.description,
+  });
 
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formDataToSend = new FormData();
+    formDataToSend.append('username', formData.username);
+    formDataToSend.append('description', formData.description);
 
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      const formDataToSend = new FormData();
-      formDataToSend.append('username', formData.username);
-      formDataToSend.append('description', formData.description);
-      try {
-        const response = await fetch(`/api/user/${sessionUserData.user.id}`, {
-          method: "PUT",
-          body: formDataToSend,
-        })
-        if(response.status != 200) {
-          setStatus(await response.text())
-          return
-        }
-        
-        setStatus(await response.text())
-      } catch (error) {
-        console.log(error)
-      }
-    };
-
+    try {
+      const response = await fetch(`/api/user/${sessionUserData.user.id}`, {
+        method: "PUT",
+        body: formDataToSend,
+      });
+      setStatus(await response.text());
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-md mx-auto p-3 bg-white rounded shadow-md ">
-      {status && <p className="text-red-500 mb-4">{status}</p>}
+    <form onSubmit={handleSubmit} className="max-w-md mx-auto p-6 bg-gray-800 rounded-lg shadow-lg">
+      {status && <p className="text-gray-100 mb-4">{status}</p>}
       <div className="mb-4">
-        <label htmlFor="username" className="block text-gray-700 text-sm font-bold mb-2">
+        <label htmlFor="username" className="block text-blue-500 text-sm font-bold mb-2">
           Nome de Usuário:
         </label>
         <input 
           type="text" 
           id="username" 
           value={formData.username}
-          onChange={(e) => setFormData({...formData, username: e.target.value})} 
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          onChange={(e) => setFormData({ ...formData, username: e.target.value })} 
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-white bg-gray-700 leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
 
       <div className="mb-4">
-        <label htmlFor="description" className="block text-gray-700 text-sm font-bold mb-2">
+        <label htmlFor="description" className="block text-blue-500 text-sm font-bold mb-2">
           Descrição:
         </label>
         <textarea 
           id="description" 
           value={formData.description}
-          onChange={(e) => setFormData({...formData, description: e.target.value})} 
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          onChange={(e) => setFormData({ ...formData, description: e.target.value })} 
+          className="shadow appearance-none border rounded w-full py-2 px-3 bg-gray-700 text-white leading-tight focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
 
-<div className='grid grid-cols-2 gap-2 px-4'>
-      {/* <InputSocial onChange={() => {}} socialMediaName={"Instagram"}/>
-      <InputSocial onChange={() => {}} socialMediaName={"Discord"}/>
-      <InputSocial onChange={() => {}} socialMediaName={"Github"}/>
-      <InputSocial onChange={() => {}} socialMediaName={"Tiktok"}/> */}
+      <div className="grid grid-cols-2 gap-4">
+        <InputSocial onChange={() => {}} socialMediaName={"Instagram"} />
+        <InputSocial onChange={() => {}} socialMediaName={"Discord"} />
+        <InputSocial onChange={() => {}} socialMediaName={"Github"} />
+        <InputSocial onChange={() => {}} socialMediaName={"Tiktok"} />
+        <InputSocial onChange={() => {}} socialMediaName={"Twitter"} />
+        <InputSocial onChange={() => {}} socialMediaName={"Facebook"} />
+      </div>
 
-</div>
-
-      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+      <button type="submit" className="mt-4 w-full bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500">
         Salvar Alterações
       </button>
     </form>
